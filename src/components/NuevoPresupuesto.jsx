fae460afa72711866ff6c2d37c91fcd07e8bcbc1
Mario González <mariogonzalez@iMac-de-Mario.local>
2022-11-11 14:31:21 -0600

    input type presupuesto2

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -25,9 +25,8 @@ const NuevoPresupuesto = ({presupuesto, setPresupuesto, setIsValidPresupuesto}) 
             <label htmlFor="">Definir Presupuesto</label>
             <input 
                 className='nuevo-presupuesto'
-                
+                type="number"
                 placeholder='Agrega tu presupuesto'
-                type="text" inputmode="numeric" pattern="[0-9]*"
                 value={presupuesto}
                 onChange={ e => setPresupuesto( Number(e.target.value ) )}
              />
